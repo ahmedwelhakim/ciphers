ad8ca28a77d3c3f51b8af0c99416bd5998df84ff
ahmed <ahmed.w.elhakim@gmail.com>
2021-12-28 19:58:50 +0200

    completing playFair cipher encrypt algorithm

--- classicCiphers.cpp
+++ classicCiphers.cpp
@@ -2,14 +2,70 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include<algorithm>
+#include <algorithm>
 #include <cctype>
 using namespace std;
-// prototypes
+
+// Helper functions prototypes
 vector<string> readFileToStrings(string path);
 void saveTofile(string path, string text);
+void appendTofile(string path, string text);
+void search(char keyT[5][5], char a, char b, int arr[]);
+void toLowerCase(string *text);
+void prepareMatrixTable(string preparedKey, int *letterFrequencies, char matrix[5][5]);
+void search(char keyT[5][5], char a, char b, int arr[]);
+int mod5(int a);
 
-string CeaserCipherEncrypt(string text, int s)
+// Encryption prototypes
+string CeaserCipherEncrypt(const string text, const int s);
+string PlayFairCipherEncrypt(const string plainText, const string key);
+
+int main()
+{
+    // ------- 1 CEASAR CIPHER -------------------------------------------------------------------
+    vector<string> ceaserPlainTexts = readFileToStrings("Input Files/Caesar/caesar_plain.txt");
+    for (size_t i = 0; i < ceaserPlainTexts.size(); i++)
+    {
+        string cipher_3 = CeaserCipherEncrypt(ceaserPlainTexts[i], 3);
+        string cipher_6 = CeaserCipherEncrypt(ceaserPlainTexts[i], 6);
+        string cipher_12 = CeaserCipherEncrypt(ceaserPlainTexts[i], 12);
+        if (i == 0)
+        {
+            saveTofile("outputs/Caesar/ceaser_cipher_3.txt", cipher_3);
+            saveTofile("outputs/Caesar/ceaser_cipher_6.txt", cipher_6);
+            saveTofile("outputs/Caesar/ceaser_cipher_12.txt", cipher_12);
+        }
+        else
+        {
+            appendTofile("outputs/Caesar/ceaser_cipher_3.txt", cipher_3);
+            appendTofile("outputs/Caesar/ceaser_cipher_6.txt", cipher_6);
+            appendTofile("outputs/Caesar/ceaser_cipher_12.txt", cipher_12);
+        }
+    }
+
+    // ------- 2 PLAY FAIR CIPHER -------------------------------------------------------------------
+    vector<string> playFairPlaintexts = readFileToStrings("Input Files/PlayFair/playfair_plain.txt");
+    for (size_t i = 0; i < playFairPlaintexts.size(); i++)
+    {
+        string cipher_rats = PlayFairCipherEncrypt(playFairPlaintexts[i], "rats");
+        string cipher_archangel = PlayFairCipherEncrypt(playFairPlaintexts[i], "archangel");
+        if (i == 0)
+        {
+            saveTofile("outputs/PlayFair/playfair_cipher_rats.txt", cipher_rats);
+            saveTofile("outputs/PlayFair/playfair_cipher_archangel.txt", cipher_archangel);
+        }
+        else
+        {
+            appendTofile("outputs/PlayFair/playfair_cipher_rats.txt", cipher_rats);
+            appendTofile("outputs/PlayFair/playfair_cipher_archangel.txt", cipher_archangel);
+        }
+        
+    }
+
+    return 0;
+}
+
+string CeaserCipherEncrypt(const string text, const int s)
 {
     string result = "";
 
@@ -32,45 +88,164 @@ string CeaserCipherEncrypt(string text, int s)
 
 string PlayFairCipherEncrypt(const string plainText, const string key)
 {
-    string cipher = plainText;
+    string result = plainText;
+    string preparedKey = key;
     // prepare the plain text to be ciphered
-    for (size_t i = 0; i < cipher.size(); i++)
+    toLowerCase(&result);
+    // remove white spaces
+    result.erase(remove_if(result.begin(), result.end(), ::isspace), result.end());
+    // if two letter in couple are the same put x between them
+    for (size_t i = 0; i < result.size(); i++)
+    {
+        if (result[i] == result[i + 1] && i % 2 == 0)
+        {
+            result.insert(i + 1, "x");
+        }
+    }
+    // if size not even insert x to last letter
+    if (result.size() % 2 != 0)
+    {
+        result.insert(result.size(), "x");
+    }
+
+    // make key all lower case
+    toLowerCase(&preparedKey);
+    // letter frequency array
+    int letterFrequencies[26] = {0};
+
+    char matrix[5][5];
+
+    prepareMatrixTable(preparedKey, letterFrequencies, matrix);
+    // Now the matrix is initialized w need to search for letter position and
+    // do the playfair encryption
+    int searchedIndices[4];
+    for (size_t i = 0; i < result.size(); i += 2)
+    {
+        search(matrix, result[i], result[i + 1], searchedIndices);
+        if (searchedIndices[0] == searchedIndices[2])
+        {
+            result[i] = matrix[searchedIndices[0]][mod5(searchedIndices[1] + 1)];
+            result[i + 1] = matrix[searchedIndices[0]][mod5(searchedIndices[3] + 1)];
+        }
+        else if (searchedIndices[1] == searchedIndices[3])
+        {
+            result[i] = matrix[mod5(searchedIndices[0] + 1)][searchedIndices[1]];
+            result[i + 1] = matrix[mod5(searchedIndices[2] + 1)][searchedIndices[1]];
+        }
+        else
+        {
+            result[i] = matrix[searchedIndices[0]][searchedIndices[3]];
+            result[i + 1] = matrix[searchedIndices[2]][searchedIndices[1]];
+        }
+    }
+
+    return result;
+}
+
+void toLowerCase(string *text)
+{
+    for (size_t i = 0; i < (*(text)).size(); i++)
     {
         // Make all lower case
-        if (isupper(cipher[i]))
+        if (isupper((*(text))[i]))
         {
-            cipher[i] = tolower(plainText[i]);
-        }     
+            (*text)[i] = tolower((*text)[i]);
+        }
     }
-    // remove white spaces
-    cipher.erase(remove_if( cipher.begin(), cipher.end(),::isspace), cipher.end());
-   // if two letter in couple are the same put x between them
-    for (size_t i = 0; i < cipher.size(); i++)
-    {
-        if(cipher[i]==cipher[i+1] && i%2==0)
-        {
-            cipher.insert(i+1,"x");
-        }        
-    }
-    
-    cout<<cipher<<endl;
-
-  
-    
 }
-int main()
+void prepareMatrixTable(string preparedKey, int *letterFrequencies, char matrix[5][5])
 {
-
-    vector<string> texts = readFileToStrings("caesar_plain.txt");
-    for (size_t i = 0; i < texts.size(); i++)
+    int loopCounter = 0;
+    int alphabeticNumber = 0;
+    // Initialize the matrix with the key and alphabetics
+    for (size_t i = 0; i < 5; i++)
     {
-        string cipher = CeaserCipherEncrypt(texts[i], 3);
-        saveTofile("ceaser_cipher_3.txt", cipher);
+        for (size_t j = 0; j < 5; j++)
+        {
+            if (loopCounter < preparedKey.size())
+            {
+            LETTER_FREQUENCY:
+                if (letterFrequencies[preparedKey[loopCounter] - 97] < 1)
+                {
+                    if (preparedKey[loopCounter] != 'j')
+                    {
+                        matrix[i][j] = preparedKey[loopCounter];
+                        letterFrequencies[preparedKey[loopCounter] - 97]++;
+                        loopCounter++;
+                    }
+                    else
+                    {
+                        if (letterFrequencies['i' - 97] < 1)
+                        {
+                            matrix[i][j] = 'i';
+                            letterFrequencies['i' - 97]++;
+                            loopCounter++;
+                            continue;
+                        }
+                        loopCounter++;
+                        goto LETTER_FREQUENCY;
+                    }
+                }
+                else
+                {
+                    while (letterFrequencies[preparedKey[loopCounter] - 97] >= 1 && loopCounter < preparedKey.size())
+                    {
+                        loopCounter++;
+                    }
+                    if (loopCounter < preparedKey.size())
+                    {
+                        matrix[i][j] = preparedKey[loopCounter];
+                        letterFrequencies[preparedKey[loopCounter] - 97]++;
+                    }
+                    else
+                    {
+                        goto A;
+                    }
+                }
+            }
+            else // the key is written in the matrix
+            // the matrix should be filled with the remain alphabetic in alphabetic order
+            {
+            A:
+                while (letterFrequencies[alphabeticNumber] > 0 || (alphabeticNumber + 97) == 'j')
+                {
+                    alphabeticNumber++;
+                }
+                matrix[i][j] = (char)(alphabeticNumber + 97);
+                alphabeticNumber++;
+            }
+        }
     }
-
-    PlayFairCipherEncrypt("Hrrello World","");
-    return 0;
 }
+void search(char keyT[5][5], char a, char b, int arr[])
+{
+    int i, j;
+
+    if (a == 'j')
+        a = 'i';
+    else if (b == 'j')
+        b = 'i';
+
+    for (i = 0; i < 5; i++)
+    {
+
+        for (j = 0; j < 5; j++)
+        {
+
+            if (keyT[i][j] == a)
+            {
+                arr[0] = i;
+                arr[1] = j;
+            }
+            else if (keyT[i][j] == b)
+            {
+                arr[2] = i;
+                arr[3] = j;
+            }
+        }
+    }
+}
+int mod5(int a) { return (a % 5); }
 vector<string> readFileToStrings(string path)
 {
     vector<string> output;
@@ -92,6 +267,23 @@ vector<string> readFileToStrings(string path)
     return output;
 }
 void saveTofile(string path, string text)
+{
+    // Create and open a text file
+    ofstream MyFile(path);
+
+    // Write to the file
+    if (MyFile.is_open())
+    {
+        MyFile << text << endl;
+    }
+    else
+    {
+        cout << "ERROR writing to file";
+    }
+    // Close the file
+    MyFile.close();
+}
+void appendTofile(string path, string text)
 {
     // Create and open a text file
     ofstream MyFile(path, ios_base::app);
