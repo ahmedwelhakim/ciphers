0e5ec23e39864be0eb1e5a9d8f9064e62b69b0c5
ahmed <ahmed.w.elhakim@gmail.com>
2022-01-01 16:25:37 +0200

    adding .h files to all of them

---
+++ AES/AES.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include<bitset>
+#include"AES.h"
+using namespace std;
+
+int main()
+{
+    cout<<(int)S_Box[0][0];
+    return 0;
+}
--- classic ciphers/classicCiphers.cpp
+++ classic ciphers/classicCiphers.cpp
@@ -4,31 +4,8 @@
 #include <vector>
 #include <algorithm>
 #include <cctype>
+#include"classicCiphers.h"
 using namespace std;
-
-#define REPEATING_MODE false
-#define AUTO_MODE true
-
-// Helper functions prototypes
-vector<string> readFileToStrings(string path);
-void saveTofile(string path, string text);
-void appendTofile(string path, string text);
-void search(char keyT[5][5], char a, char b, int arr[]);
-void toLowerCase(string *text);
-void toUpperCase(string *text);
-void prepareMatrixTable(string preparedKey, int *letterFrequencies, char matrix[5][5]);
-void search(char keyT[5][5], char a, char b, int arr[]);
-int mod5(int a);
-int mod26(int a);
-void removeSpaces(string *s);
-vector<vector<int>> multiply(vector<vector<int>> m1, vector<vector<int>> m2);
-
-// Encryption prototypes
-string CeaserCipherEncrypt(const string text, const int s);
-string PlayFairCipherEncrypt(const string plainText, const string key);
-string HillCipherEncrypt(const string plain, const vector<vector<int>> key);
-string VigenerCipherEncrypt(const string plain, const string key, const bool mode);
-string VernamCipherEncrypt(const string plain, const string key);
 
 int main()
 {
